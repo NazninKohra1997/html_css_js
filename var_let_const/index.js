7fca741ac4e208aaafdae82d0cd68807d8fbac3b
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-15 08:55:47 +0600

    I added a new file

---
+++ var_let_const/index.js
@@ -0,0 +1,25 @@
+var a = 22;
+var a = 34;
+console.log(a);
+
+{
+    var a = 70;
+    console.log(a);
+}
+
+
+console.log();
+
+let b = 60;
+ b = 22;
+ console.log(b);
+
+ {
+    let b = 44;
+    console.log(b);
+ }
+
+console.log()
+
+ const c = 89;
+ console.log(c);
