fc9a97fd832cd1089bfc10c14ea385a2f7992c59
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-16 11:00:46 +0600

    I added two files

---
+++ Primitive_data_type/index.js
@@ -0,0 +1,14 @@
+let n1 = null;
+let n2 = 367;
+
+let s1 = "hey Jhora";
+let s2 = Symbol("This is a symbol");
+
+let b1 = BigInt(55) + BigInt(5);
+let b2 = true;
+
+let u = undefined;
+let u1;
+
+console.log(n1,n2,s1,s2,b1,b2,u,u1);
+console.log(typeof u1);
---
+++ non-premitive-object/index.js
@@ -0,0 +1,9 @@
+const item = {
+    "Jhora" : 305,
+     "Raisa" : true,
+    "Raina" : Symbol("She is cute")
+} 
+
+console.log(item["Raisa"])
+console.log(item["Raina"])
+
