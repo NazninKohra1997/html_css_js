81609af0a5cd3c79579e55eee25416b81372f86f
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-19 10:30:08 +0600

    I added some files

---
+++ Chap-02/comparison_operator/index.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 6;
+
+console.log("a==b",a==b)
+console.log("a!=b",a!=b)
+console.log("b>a",b>a)
+console.log("b>=a",b>=a)
+
+
+let c = 7;
+let d = "7";
+
+console.log("a===b",a===b)
+console.log("a!==b",a!==b)
+
+
+
---
+++ Chap-02/increment_decrement/index.js
@@ -0,0 +1,18 @@
+let a = 11;
+
+console.log("++a ", ++a)
+console.log("a",a)
+
+let b = 12;
+
+console.log("b++",b++)
+console.log("b", b)
+
+let c = 5;
+console.log("c--",c--)
+console.log("c", c)
+
+let d = 9;
+console.log("--d",--d)
+console.log("d", d)
+
