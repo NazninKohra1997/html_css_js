0ac38b1b418c8fb317562136f6dd0533b053515e
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-09-21 10:49:12 +0600

    I added two new files

---
+++ Chap-03/function_standard/index.js
@@ -0,0 +1,14 @@
+const avg = (a,b) =>{
+    return a*b;
+}
+
+let s = avg(5,7);
+console.log(s);
+
+
+
+const fo = () =>{
+    console.log("Hey! I'm doing js");
+}
+
+fo();
---
+++ Chap-03/funtion_old/index.js
@@ -0,0 +1,14 @@
+function func(x,y){
+    return (x+y)/2;
+}
+
+let f = func(8,2);
+console.log(f);
+
+
+
+function foo(){
+    console.log("This is a function");
+}
+
+foo();
